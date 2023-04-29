2b0b5c0a3433648b891668a1f51a0da289a22745
lpreimesberger <lpreimesberger@gmail.com>
2023-04-29 14:48:03 -0500

    - playamon

--- lib/scavenge.dart
+++ lib/scavenge.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 import 'package:convert/convert.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+/*
+  this is the pokemon page - we want to let people 'capture' other
+  hoomans or artwork in their 2023 purrse
+
+ */
 class ScavengePage extends StatefulWidget {
   const ScavengePage({super.key, required this.title});
 
@@ -12,142 +19,83 @@ class ScavengePage extends StatefulWidget {
 }
 
 class _ScavengePageState extends State<ScavengePage> {
-
+  late SharedPreferences prefs;
   ValueNotifier<dynamic> result = ValueNotifier(null);
-  final _formKey = GlobalKey<FormState>();
-  String playaName = '';
-  String emailAddress = '';
+  List<String> found = [];
 
-  var pattern = '[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}';
+  init() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      var x = prefs.getStringList('found2023');
+      if (x != null) {
+        found = x;
+      }
+    });
+  }
 
+  _ScavengePageState() {
+    init();
+  }
+
+  Widget _myListView(BuildContext context) {
+    return ListView.separated(
+      itemCount: found.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(found[index]),
+          tileColor:
+              found[index].contains('hooman') ? Colors.grey : Colors.deepPurple,
+          leading: found[index].contains('hooman')
+              ? const Icon(Icons.person)
+              : const Icon(Icons.house_outlined),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const Divider();
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Purr2Purr NFC Helper')),
+        appBar: AppBar(
+            title: const Text('Your Purrse'), backgroundColor: Colors.black),
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: FutureBuilder<bool>(
             future: NfcManager.instance.isAvailable(),
             builder: (context, ss) => ss.data != true
-                ? Center(child: Text('NfcManager.isAvailable(): ${ss.data}'))
+                ? const Center(child: Text('Oops - no NFC scanner found :('))
                 : Flex(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              direction: Axis.vertical,
-              children: [
-                Form(
-                    key: _formKey,
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          TextFormField(
-                            // The validator receives the text that the user has entered.
-                            decoration: const InputDecoration(
-                              icon: Icon(Icons.person),
-                              hintText: 'Playa Name?',
-                              labelText: 'Playa Name *',
-                            ),
-                            onChanged: (value) {
-                              setState(() {
-                                playaName = value;
-                              });
-
-
-                            },
-                            onSaved: (String? value) {
-                              if(value != null){
-                                playaName = value;
-                              }
-                            },
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter some text';
-                              }
-                              return null;
-                            },
-                          ),
-                          TextFormField(
-                            // The validator receives the text that the user has entered.
-                            decoration: const InputDecoration(
-                              icon: Icon(Icons.email),
-                              hintText: 'Optional email',
-                              labelText: 'Email *',
-                            ),
-                            onChanged: (value) {
-                              final regex = RegExp(pattern);
-                              if(regex.hasMatch(value)){
-                                emailAddress = value;
-                              }
-                            },
-                            onSaved: (String? value) {
-                              final regex = RegExp(pattern);
-                              if(value != null && regex.hasMatch(value)){
-                                emailAddress = value;
-                              }
-                            },
-                            validator: (value) {
-                              final regex = RegExp(pattern);
-                              if (value != null && regex.hasMatch(value)) {
-                                return 'Please enter some text';
-                              }
-                              return null;
-                            },
-                          ),
-
-                          Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 16.0),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  // Validate returns true if the form is valid, or false otherwise.
-                                  if (_formKey.currentState!.validate() && playaName.isNotEmpty ) {
-                                    // If the form is valid, display a snackbar. In the real world,
-                                    // you'd often call a server or save the information in a database.
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(content: Text('Processing Data')),
-                                    );
-                                  }
-                                },
-                                child: const Text('Arm'),
-                              )),
-                        ])
-
-                ),
-
-                Flexible(
-                  flex: 2,
-                  child: Container(
-                    margin: EdgeInsets.all(4),
-                    constraints: BoxConstraints.expand(),
-                    decoration: BoxDecoration(border: Border.all()),
-                    child: SingleChildScrollView(
-                      child: ValueListenableBuilder<dynamic>(
-                        valueListenable: result,
-                        builder: (context, value, _) =>
-                            Text('${value ?? ''}'),
-                      ),
-                    ),
-                  ),
-                ),
-                Flexible(
-                  flex: 3,
-                  child: GridView.count(
-                    padding: EdgeInsets.all(4),
-                    crossAxisCount: 2,
-                    childAspectRatio: 4,
-                    crossAxisSpacing: 4,
-                    mainAxisSpacing: 4,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    direction: Axis.vertical,
                     children: [
-                      ElevatedButton(
-                          child: Text('Read Chip'), onPressed: _tagRead),
-                      playaName.isNotEmpty ? ElevatedButton(
-                          child: Text('Burn Chip'),
-                          onPressed: _ndefWrite) : Spacer(),
+                      Flexible(
+                        flex: 10,
+                        child: _myListView(context),
+                      ),
+                      Flexible(
+                        flex: 3,
+                        child: GridView.count(
+                          padding: const EdgeInsets.all(4),
+                          crossAxisCount: 1,
+                          childAspectRatio: 6,
+                          crossAxisSpacing: 4,
+                          mainAxisSpacing: 1,
+                          children: [
+                            ElevatedButton(
+                                onPressed: _tagRead,
+                                child: const Text('Read Chip')),
+                            ElevatedButton(
+                                onPressed: _reset,
+                                child: const Text('Clear database')),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
-                ),
-              ],
-            ),
           ),
         ),
       ),
@@ -164,55 +112,76 @@ class _ScavengePageState extends State<ScavengePage> {
         return;
       }
       var hooman = hex.encode(ndef.additionalData['identifier']);
-      var xnm = '';
-      var xyr = '2023';
-      var xis = 'losers';
-      if(ndef.cachedMessage != null){
+      var poi = '';
+      var loi = '';
+      if (ndef.cachedMessage != null) {
         ndef.cachedMessage?.records.forEach((element) {
           // there are code/byte markers in front
           var swapBack = String.fromCharCodes(element.payload).substring(3);
-          if(swapBack.startsWith("xnm:")){
-            xnm = swapBack.substring(4);
+          if (swapBack.startsWith("xnm:")) {
+            poi = swapBack.substring(4);
           }
-          if(swapBack.startsWith("xis:")){
-            xis = swapBack.substring(4);
-          }
-          if(swapBack.startsWith("xyr:")){
-            xyr = swapBack.substring(4);
+          if (swapBack.startsWith("loi:")) {
+            loi = swapBack.substring(4);
           }
         });
       }
-      result.value = "xid:$hooman\nxnm:$xnm\nxis:$xis\nxyr:$xyr";
+      result.value = "xid:$hooman\npoi:$poi\nloi:$loi\n";
       NfcManager.instance.stopSession();
-    });
-  }
-
-  void _ndefWrite() {
-    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-      var ndef = Ndef.from(tag);
-      if (ndef == null || !ndef.isWritable) {
-        result.value = 'Tag is not ndef writable';
-        NfcManager.instance.stopSession(errorMessage: result.value);
-        return;
+      if (poi != '') {
+        remember('hooman:$poi');
+        popup('Woot!  You captured a wild $poi!');
       }
-
-      NdefMessage message = NdefMessage([
-        NdefRecord.createText('xnm:$playaName'),
-        NdefRecord.createText('xyr:2023'),
-        NdefRecord.createText('xis:pussy_avalanche'),
-      ]);
-
-      try {
-        await ndef.write(message);
-        result.value = 'Success to "Ndef Write"';
-        NfcManager.instance.stopSession();
-      } catch (e) {
-        result.value = e;
-        NfcManager.instance.stopSession(errorMessage: result.value.toString());
-        return;
+      if (loi != '') {
+        remember('location:$loi');
+        popup('Fuck yeah! $loi is yours!');
       }
     });
   }
 
+  void remember(String thisMessage) async {
+    if (found.contains(thisMessage)) {
+      print("already found");
+      return;
+    }
+    found.add(thisMessage);
+    prefs.setStringList("found2023", found);
+    setState(() {
+      found = prefs.getStringList('found2023')!;
+    });
+    print(thisMessage);
+  }
 
+  Future<void> popup(String thisMessage) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('OMFG!'),
+          content: SingleChildScrollView(
+            child: ListBody(children: <Widget>[
+              Text(thisMessage),
+            ]),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Woo!'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _reset() async {
+    found.clear();
+    prefs.setStringList("found2023", []);
+    setState(() {
+      found = prefs.getStringList('found2023')!;
+    });
+  }
 }
